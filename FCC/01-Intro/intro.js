67cdd9eb18d4f4b54ef65841a2b6084d1055c98a
MarsIfeanyi <marschalice@gmail.com>
2022-11-29 18:02:30 +0100

    Initial Commit

---
+++ FCC/01-Intro/intro.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * TypeScript is all about type safety
+ *
+ * What TypeScript does:
+ *
+ * 1. Static Checking: Whenever you are writing code the syntax is constantly being checked by the IDE
+ *
+ * Hint: The whole job of TypeScript is simply to analyze the code as we type.
+ *
+ * Typescript is a development tool, your code still runs on pure JS
+ *
+ *
+ *https://www.typescriptlang.org/
+ *
+ *
+ *
+ * INSTALLATION OF TYPESCRIPT
+ *
+ * First you will install typescript globally, then as you work on a project you will install it as a dev dependency.
+ *
+ * sudo npm install -g typescript
+ *
+ * */
+var user = { name: "Mars", age: 10 };
+console.log("Mars Ifeanyi");
+console.log(user.name);
+// Hint: to run the code use tsc fileName ie tsc intro.ts
