cdcac31ce84e9f7c2056275c5ce2e31f1105dd68
MarsIfeanyi <marschalice@gmail.com>
2022-11-29 18:01:25 +0100

    Initial Commit

---
+++ FCC/01-Intro/08-type-Aliases.ts
@@ -0,0 +1,17 @@
+//Hint: type is a keyword in TS
+
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+// using the type...
+
+function createUser(user: User): User {
+  return { name: "", email: "", isActive: true };
+}
+
+createUser({ name: "", email: "", isActive: true });
+
+//Hint:type Aliases helps you to just define a single type then use use it in your functions or anywhere more than once...That's the type you defined can be multi-used in functions...
