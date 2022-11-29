4c584bfd64673852993b7b31960237ea9d3ef318
MarsIfeanyi <marschalice@gmail.com>
2022-11-29 18:00:20 +0100

    Initial Commit

---
+++ FCC/01-Intro/03-primitive-types.ts
@@ -0,0 +1,29 @@
+/**
+ * In TS, the primitive types are:
+ * 1. String
+ * 2. Number
+ * 3. Boolean
+ */
+
+// string
+
+let greetings: string = "Hello Mars";
+
+greetings.toUpperCase;
+console.log(greetings);
+
+// number
+
+let userId: number = 334567.957;
+
+// Using type inference...Hint: This is a better way to write clean code
+
+let userId2 = 3335555;
+userId2.toFixed();
+
+// Boolean
+let isLoggedIn: boolean = false;
+
+// Hint: You can use the dot notation to see the method available on each of the data types...The method varies
+
+export {};
