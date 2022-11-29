915c03466368987a5b24c33df8afd5e7c617fd82
MarsIfeanyi <marschalice@gmail.com>
2022-11-29 18:04:44 +0100

    Initial Commit

---
+++ FCC/02-Setup-and-Project/src/18-getters-and-setters.ts
@@ -0,0 +1,36 @@
+class User4 {
+  private _courseCount = 1;
+
+  readonly city: string = "Wall Street";
+  constructor(
+    public email: string,
+    public name: string,
+    private userId: string
+  ) {}
+
+  // private method
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  // creating a getter function
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  //Creating a setter function...Hint: a setter function does not return any value and thus you don't add any type to it.
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1 ");
+    }
+    this._courseCount = courseNum;
+  } //Hint: The logic here is upto but the most important thing is that in setter function there is no "return type"
+}
+
+//Creating object from the class
+const mars4 = new User4("mars@g.com", "Charles", "2020");
