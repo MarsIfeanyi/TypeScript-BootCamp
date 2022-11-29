382eae479df8d032bc929b692546305743fdf992
MarsIfeanyi <marschalice@gmail.com>
2022-11-29 18:04:26 +0100

    Initial Commit

---
+++ FCC/02-Setup-and-Project/src/16-index.ts
@@ -0,0 +1,17 @@
+console.log("TypeScript is Here");
+
+console.log("Web3-Blockchain");
+
+// Class in TypeScript
+
+class User {
+  email: string;
+  name: string;
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+//Creating object from the class
+const mars = new User("mars@g.com", "Charles");
