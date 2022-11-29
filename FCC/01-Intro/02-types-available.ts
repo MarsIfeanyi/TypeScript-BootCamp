b5baf1910f9af1e720c80879e1cb7c73b538e235
MarsIfeanyi <marschalice@gmail.com>
2022-11-29 18:00:12 +0100

    Initial Commit

---
+++ FCC/01-Intro/02-types-available.ts
@@ -0,0 +1,23 @@
+/**
+ * The various types available in TypeScript are:
+ *
+ * 1. Number
+ * 2. String
+ * 3. Boolean
+ * 4. Null
+ * 5. Undefined
+ * 6. Void
+ * 7. Object
+ * 8. Arrays
+ * 9. Tuples
+ * 10. Any
+ * 11. Never
+ * 12. Unknown etc
+ *
+ *
+ *
+ * Hint: The Syntax:
+ *
+ * let variableName:type = value
+ *
+ */
