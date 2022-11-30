f25b0de0a64b9e75cca4b3fcb432786f536d249c
MarsIfeanyi <marschalice@gmail.com>
2022-11-30 20:56:55 +0100

    Initial Commit

---
+++ FCC/02-Setup-and-Project/dist/28-DiscriminatedUnion-and-Exhaustiveness-Checking-with-never.js
@@ -0,0 +1,21 @@
+"use strict";
+//Discriminated Union
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    return shape.side * shape.side;
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultForShape = shape;
+            return _defaultForShape;
+    }
+}
