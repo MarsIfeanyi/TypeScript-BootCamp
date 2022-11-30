6da3768a0072a403e2b066ffac2f9da651f88100
MarsIfeanyi <marschalice@gmail.com>
2022-11-30 20:53:46 +0100

    Initial Commit

---
+++ FCC/02-Setup-and-Project/src/28-DiscriminatedUnion-and-Exhaustiveness-Checking-with-never.ts
@@ -0,0 +1,45 @@
+//Discriminated Union
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  return shape.side * shape.side;
+}
+
+//Exhaustive checking with never
+
+type Shape2 = Circle | Square | Rectangle;
+
+function getArea(shape: Shape2) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side * shape.side;
+    case "rectangle":
+      return shape.length * shape.width;
+
+    default:
+      const _defaultForShape: never = shape;
+      return _defaultForShape;
+  }
+}
