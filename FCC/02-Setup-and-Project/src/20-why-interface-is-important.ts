f9176a352e2938a27cf31f2e8fc2c1c13a25c6cd
MarsIfeanyi <marschalice@gmail.com>
2022-11-30 20:52:17 +0100

    Initial Commit

---
+++ FCC/02-Setup-and-Project/src/20-why-interface-is-important.ts
@@ -0,0 +1,34 @@
+//With interface you can define a general features and thus anyone that wants to use the interface must implement all the features you defined in the interface
+
+interface TakePhoto {
+  cameraMode: string;
+  filter: string;
+  burst: number;
+}
+
+interface Story {
+  createStory(): void;
+}
+
+class Instagram implements TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {}
+}
+
+class Youtube implements TakePhoto, Story {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number,
+    public short: string
+  ) {}
+
+  createStory(): void {
+    console.log("Story was created");
+  }
+}
+
+//Hint: You must always implement everything defined in  the interface, then you can add your methods, properties or logics as well.
