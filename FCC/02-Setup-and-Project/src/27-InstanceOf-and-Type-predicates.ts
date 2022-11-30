a402132b1b3ea701d6256f6f8d8fc3190a81f025
MarsIfeanyi <marschalice@gmail.com>
2022-11-30 20:53:38 +0100

    Initial Commit

---
+++ FCC/02-Setup-and-Project/src/27-InstanceOf-and-Type-predicates.ts
@@ -0,0 +1,30 @@
+//typeof checks for type, while instanceOf checks if it an instance of a class
+
+// const date = new Date();
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+} //Hint:Date is a class is JS
+
+//Type predicates
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
+  }
+}
