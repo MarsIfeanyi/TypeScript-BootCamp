b6cc00a9379f6e292497307e5b1d0464e71e3ae1
MarsIfeanyi <marschalice@gmail.com>
2023-03-30 20:39:40 +0100

    Initial Commit

---
+++ Understanding-TypeScript/02-TypeScript-Basics_and_Basic-Types/05-object-types/app.ts
@@ -0,0 +1,18 @@
+// Explicitly declaring/ assigning the  object type.
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+
+// const person = {
+//   name: "Maximilian",
+//   age: 30,
+// };
+
+// Implicitly  assigning the object type by TypeScript (inference )
+const person = {
+  name: "MarsIfeanyi",
+  age: 25,
+}; //Hint: Here TypeScript will inference it as "key-type" pair instead of "key-value" pair
+
+console.log(person.name);
