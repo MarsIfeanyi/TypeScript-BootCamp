c1380bb415094bcdc95cd9b46c406342a85159c2
MarsIfeanyi <marschalice@gmail.com>
2023-04-02 19:52:15 +0100

    Initial Commit

---
+++ Understanding-TypeScript/02-TypeScript-Basics_and_Basic-Types/12-function-types/app.ts
@@ -0,0 +1,36 @@
+/**
+ * 
+ *FUNCTION TYPE AND CALLBACK
+
+ Function type defines the parameters and the return type of a function
+ */
+
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+} //Hint: cb = callback function
+
+printResult(add(5, 12));
+
+// CReating function type
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
+
+// let someValue: undefined;
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
