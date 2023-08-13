6d585253034b177809731c89bcc2ea1a83507867
MarsIfeanyi <marschalice@gmail.com>
2023-08-13 16:48:09 +0100

    Update file

--- FCC/01-Intro/06-functions.js
+++ FCC/01-Intro/06-functions.js
@@ -15,3 +15,35 @@ addTwo(5);
 getUpper("mars");
 signUpUser("mars", "mars@gmail.com", false);
 loginUser("mars", "mars@gmail.com");
+//EXplicitly declaring the type in an arrow function
+var getHello = function (s) {
+    var hello = s;
+    return hello;
+};
+var hello = getHello("Hello Mars");
+console.log("====================================");
+console.log(hello);
+console.log("====================================");
+// String mappings
+var heros = ["thor", "spiderman", "ironman"];
+heros.map(function (hero) {
+    var theHeros = "hero is ".concat(hero);
+    console.log(theHeros);
+    return theHeros;
+});
+// number mappings
+var accounts = [100, 300, 500, 60, 30];
+accounts.map(function (account) {
+    return account;
+});
+//when the function is not returning anything, then use Void
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+/**The never type represents values which are never observed.  in a return type, this means that the function throws an exception or terminates execution of the program
+ *
+ * never also appears when typescript determines there's nothing left in a union
+ */
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
