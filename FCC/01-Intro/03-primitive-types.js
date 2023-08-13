9a12b74d0fc603c18ff3b30aa818083e69dd74dd
MarsIfeanyi <marschalice@gmail.com>
2023-08-13 16:47:34 +0100

    Update file

---
+++ FCC/01-Intro/03-primitive-types.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * In TS, the primitive types are:
+ * 1. String
+ * 2. Number
+ * 3. Boolean
+ */
+exports.__esModule = true;
+// string
+var greetings = "Hello Mars";
+greetings.toUpperCase;
+console.log(greetings);
+// number
+var userId = 334567.957;
+// Using type inference...Hint: This is a better way to write clean code
+var userId2 = 3335555;
+userId2.toFixed();
+// Boolean
+var isLoggedIn = false;
+isLoggedIn.valueOf();
