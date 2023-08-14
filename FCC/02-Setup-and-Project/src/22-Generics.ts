9057863c19cb2c3acce438083d119f7ac29dbcc1
MarsIfeanyi <marschalice@gmail.com>
2023-08-14 06:50:30 +0100

    Update file

--- FCC/02-Setup-and-Project/src/22-Generics.ts
+++ FCC/02-Setup-and-Project/src/22-Generics.ts
@@ -3,8 +3,7 @@
 
 In languages like C# and Java, one of the main tools in the toolbox for creating reusable components is generics, that is, being able to create a component that can work over a variety of types rather than a single one. This allows users to consume these components and use their own types.
 
-
- */
+*/
 
 const score: Array<number> = [];
 const names: Array<string> = [];
